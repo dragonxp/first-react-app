f3531365b9ec5ea4dc14aa2746a4fb65c2fc82ed
drogan xp <goodmsaul@gmail.com>
2022-07-17 09:13:09 +0545

    Revert "changed useState to useRef in NewEventForm js"
    This reverts commit e9c043243c088504842dff8260d3c432985038d4.

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,21 +1,21 @@
-import { useRef } from 'react'
+import { useState } from 'react'
 import './NewEventForm.css'
 
 export default function NewEventForm({ addEvent }) {
-    const title = useRef()
-    const date = useRef()
+    const [title, setTitle] = useState('')
+    const [date, setDate] = useState('')
 
     const resetForm = () => {
-        title.current.value = ''
-        date.current.value = ''
+        setTitle('')
+        setDate('')
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
 
         const event = {
-            title: title.current.value, 
-            date: date.current.value, 
+            title: title, 
+            date: date, 
             id: Math.floor(Math.random() * 10000)
         }
 
@@ -29,14 +29,16 @@ export default function NewEventForm({ addEvent }) {
                 <span>Event Title : </span>
                 <input 
                     type="text" 
-                    ref={title}
+                    onChange={e => setTitle(e.target.value)} 
+                    value={title}
                 />
             </label>
             <label>
                 <span>Event Date : </span>
                 <input 
                     type="date" 
-                    ref={date}
+                    onChange={e => setDate(e.target.value)} 
+                    value={date}
                 />
             </label>
             <button>Submit</button>
